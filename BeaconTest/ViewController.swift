0cd78b6f2db65cf3fceca272db14d9bd3c3de0e0
Henk van der Spek <henk@vanmezelf.nl>
2016-04-11 22:58:07 +0200

    Initial commit

---
+++ BeaconTest/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  BeaconTest
+//
+//  Created by Henk van der Spek on 11/04/16.
+//  Copyright © 2016 vanmezelf.nl. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import CoreBluetooth
+
+class ViewController: UITableViewController {
+    let major = CLBeaconMajorValue(1)
+    let minor = CLBeaconMinorValue(1)
+    let uuid = NSUUID()
+    var peripheral: CBPeripheralManager?
+    var beacon: CLBeaconRegion?
+    var data: NSDictionary?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        peripheral = CBPeripheralManager(delegate: self, queue: nil)
+        beacon = CLBeaconRegion(proximityUUID: uuid, major: major, minor: minor, identifier: "nl.vanmezelf.BeaconTest")
+        data = beacon!.peripheralDataWithMeasuredPower(nil) as NSDictionary
+    }
+}
+
+extension ViewController: CBPeripheralManagerDelegate {
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager) {
+        switch peripheral.state {
+        case .PoweredOn:
+            peripheral.startAdvertising(data as? [String : AnyObject])
+        default:
+            print("Peripheral manager did update state(\(peripheral.state.rawValue))")
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+use_frameworks!
+inhibit_all_warnings!
+
+link_with 'BeaconTest'
+
+pod 'PubNub'
